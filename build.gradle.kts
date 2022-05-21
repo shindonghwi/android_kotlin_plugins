7c9fb3d7e33b39c45b392119c9fea775b6d0c60e
shindonghwi <ehdgnl8940@naver.com>
2022-05-21 22:07:36 +0900

    feat: Intellij path에 Android Studio Path를 정의 ( Android Studio를 열리게 하기 위함 )

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,8 @@ intellij {
 
     // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
     plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
+
+    intellij.localPath.set(properties("StudioRunPath")) //localPath 를 추가 해줘야 intellij가 아닌 Android Studio 가 열림
 }
 
 // Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
