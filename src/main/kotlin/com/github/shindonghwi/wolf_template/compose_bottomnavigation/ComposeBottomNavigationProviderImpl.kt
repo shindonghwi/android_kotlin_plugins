7cf380c32438c5e91fbc25a22434118d41073900
shindonghwi <ehdgnl8940@naver.com>
2022-05-31 17:10:55 +0900

    fix: 반환 템플릿 compose bottom nav templete으로 변경

--- src/main/kotlin/com/github/shindonghwi/wolf_template/compose_bottomnavigation/ComposeBottomNavigationProviderImpl.kt
+++ src/main/kotlin/com/github/shindonghwi/wolf_template/compose_bottomnavigation/ComposeBottomNavigationProviderImpl.kt
@@ -2,8 +2,7 @@ package com.github.shindonghwi.wolf_template.compose_bottomnavigation
 
 import com.android.tools.idea.wizard.template.Template
 import com.android.tools.idea.wizard.template.WizardTemplateProvider
-import com.github.shindonghwi.wolf_template.mvvm.mvvmWizardTemplate
 
 class ComposeBottomNavigationProviderImpl  : WizardTemplateProvider() {
-    override fun getTemplates(): List<Template> = listOf(mvvmWizardTemplate)
+    override fun getTemplates(): List<Template> = listOf(composeBottomNavigationWizardTemplate)
 }
