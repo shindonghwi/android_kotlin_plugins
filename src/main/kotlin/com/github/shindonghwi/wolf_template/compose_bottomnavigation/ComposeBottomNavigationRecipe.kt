d6b8161eb92db47a324dc8ea008b87d39d68ffdd
shindonghwi <ehdgnl8940@naver.com>
2022-06-01 11:05:10 +0900

    feat: Compose Bottom Navigation Recipe 생성

---
+++ src/main/kotlin/com/github/shindonghwi/wolf_template/compose_bottomnavigation/ComposeBottomNavigationRecipe.kt
@@ -0,0 +1,14 @@
+package com.github.shindonghwi.wolf_template.compose_bottomnavigation
+
+import com.android.tools.idea.wizard.template.ModuleTemplateData
+import com.android.tools.idea.wizard.template.RecipeExecutor
+
+fun RecipeExecutor.composeBottomNavigationSetup(
+    moduleTemplateData: ModuleTemplateData,
+    value: String,
+    value1: String,
+    value2: String,
+    value3: String
+) {
+
+}
